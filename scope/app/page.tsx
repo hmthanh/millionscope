4c748543734d5f746d7224abda12d3d043ef9af9
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-04-26 02:24:45 +0700

    add docs

--- scope/app/page.tsx
+++ scope/app/page.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="nx-flex nx-min-h-screen nx-flex-col nx-items-center nx-justify-between p-24">
+    
       <div className="nx-z-10 nx-w-full nx-max-w-5xl nx-items-center nx-justify-between nx-font-mono nx-text-sm nx-lg:flex">
        Hello
       </div>
-    </main>
   );
 }
