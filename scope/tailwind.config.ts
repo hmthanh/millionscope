c91b3c2d3918ea528d06c362d1065e168adb6433
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-04-25 12:26:07 +0700

    add mdx

--- scope/tailwind.config.ts
+++ scope/tailwind.config.ts
@@ -6,15 +6,49 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: 'nx-',
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem'
+    },
+    letterSpacing: {
+      tight: '-0.015em'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      // gray: colors.gray,
+      // slate: colors.slate,
+      // neutral: colors.neutral,
+      // red: colors.red,
+      // orange: colors.orange,
+      // blue: colors.blue,
+      // yellow: colors.yellow,
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        dark: '#111'
+      }
     },
   },
   plugins: [],
+  darkMode: ['class', 'html[class~="dark"]']
 };
 export default config;
