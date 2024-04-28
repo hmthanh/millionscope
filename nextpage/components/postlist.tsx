97a7acf50d3f76cd50a4a67709b42e99855db512
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2024-04-28 20:58:55 +0700

    update tag

--- nextpage/components/postlist.tsx
+++ nextpage/components/postlist.tsx
@@ -46,7 +46,7 @@ export const PostList: React.FC<PostListProps> = ({ posts }) => {
                                     {post.tags.map((tag, index) => {
                                         return (
                                             <li key={index}>
-                                                <Tag href={`/posts/tagged/${slugify(tag)}`}>{tag}</Tag>
+                                                <Tag href={`/posts/tag/${slugify(tag)}`}>{tag}</Tag>
                                             </li>
                                         );
                                     })}
